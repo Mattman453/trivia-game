60c35a98d4db620205b69da0e7028cbdcf62ef0c
Mattman453 <mattjcruickshank@gmail.com>
2025-01-13 10:27:18 -0500

    Added basic function stubs for creating a question, adding audio, adding video, and playing audio

--- src/trivia/section/questions/mod.rs
+++ src/trivia/section/questions/mod.rs
@@ -1,8 +1,48 @@
 use std::fs::File;
 
-pub(super) struct Question {
+pub(in super) struct Question {
+
     question: String,
     answer: String,
-    audio: File,
-    video: File,
+    audio: Option<File>,
+    video: Option<File>,
+}
+
+impl Question {
+
+    fn new(question: String, answer: String) -> Self {
+
+        let mut question = Question {question, answer, audio: None, video: None};
+        question
+    }
+
+    fn add_audio(&mut self, audio: File) {
+
+        self.audio = Some(audio);
+    }
+
+    fn add_video(&mut self, video: File) {
+
+        self.video = Some(video);
+    }
+
+    // TODO: Log failed uses instead of printing to the console
+    /*
+    This function plays the audio connected to a question. If no audio is attached, a message will
+    be printed to the console.
+     */
+    fn play_audio(&mut self) {
+
+        if self.audio.is_none() {
+
+            println!("No audio attached to question.");
+            return;
+        }
+
+        if self.audio.is_some() {
+
+            // TODO: Add implementation for playing audio files with rodio
+            return;
+        }
+    }
 }
