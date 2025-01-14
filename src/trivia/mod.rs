cd393ac6b8af4ede41b559eb1dd58f0eac969dd8
Mattman453 <mattjcruickshank@gmail.com>
2025-01-14 14:57:30 -0500

    Added constructor for the Trivia struct

--- src/trivia/mod.rs
+++ src/trivia/mod.rs
@@ -1,6 +1,16 @@
+use crate::trivia::section::Section;
+
 mod section;
 
 pub(super) struct Trivia {
     title: String,
-    section: Vec<section::Section>,
+    sections: Vec<Section>,
+}
+
+impl Trivia {
+    fn new(title: String) -> Self {
+        let mut sections: Vec<Section> = Vec::new();
+        let mut trivia = Trivia{title, sections};
+        trivia
+    }
 }
