3ab1b0d68862af7e94454fa38262a9d3341c4c57
Mattman453 <mattjcruickshank@gmail.com>
2025-01-14 14:35:23 -0500

    Added creator for a new section of questions

--- src/trivia/section/mod.rs
+++ src/trivia/section/mod.rs
@@ -1,7 +1,24 @@
+use crate::trivia::section::questions::Question;
+
 mod questions;
 
 pub(super) struct Section {
     name: String,
     rules: String,
     questions: Vec<questions::Question>,
+}
+
+impl Section {
+    /*
+    This function creates a new section struct of a trivia game.
+     */
+    fn new(name: String, rules: String) -> Self {
+        let mut questions: Vec<Question> = Vec::new();
+        let mut section: Section = Section {
+            name,
+            rules,
+            questions,
+        };
+        section
+    }
 }
