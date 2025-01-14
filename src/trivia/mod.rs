f4e14e215351e0c0f3d67d136b2d5efd55918294
Mattman453 <mattjcruickshank@gmail.com>
2025-01-14 15:24:28 -0500

    Changed visibility of functions and modules

--- src/trivia/mod.rs
+++ src/trivia/mod.rs
@@ -1,6 +1,6 @@
 use crate::trivia::section::Section;
 
-mod section;
+pub mod section;
 
 /*
 This struct is the head of a trivia package. It has 2 components: title and sections. The title
@@ -8,7 +8,7 @@ component is a String meant to act as the overall name of the trivia package. Th
 is a Vector of Section structs where each Section is about a singular topic and contains the
 questions related to that topic.
  */
-pub(super) struct Trivia {
+pub struct Trivia {
     title: String,
     sections: Vec<Section>,
 }
@@ -18,9 +18,16 @@ impl Trivia {
     Constructor for a Trivia struct. Takes the title of the trivia pack and returns a Trivia struct
     containing an empty vector of sections and the title string.
      */
-    fn new(title: String) -> Self {
+    pub fn new(title: String) -> Self {
         let mut sections: Vec<Section> = Vec::new();
         let mut trivia = Trivia { title, sections };
         trivia
+    }
+
+    /*
+    This function adds a Section struct to the sections Vector.
+     */
+    pub fn add_section(&mut self, section: Section) {
+        self.sections.push(section);
     }
 }
