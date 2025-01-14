ffc49946e092ead7df91d91e39531687317acada
Mattman453 <mattjcruickshank@gmail.com>
2025-01-14 15:25:58 -0500

    Tested building a new pack, building a new section, and adding it to a pack.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 /*
 Entry point of the program. Sets up managers and overarching design.
  */
-mod trivia;
+use crate::trivia::Trivia;
+use crate::trivia::section::Section;
+
+pub mod trivia;
 
 fn main() {
-    println!("Hello, world!");
+    let mut trivia = Trivia::new("Test Pack".to_string());
+    let mut section = Section::new("Section 1".to_string(), "Their Our Know Rules".to_string());
+    trivia.add_section(section);
+
 }
